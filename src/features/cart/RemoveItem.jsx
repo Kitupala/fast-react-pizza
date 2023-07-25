65703ee4a8a0c2d187f4c0f1af04f26745c06da3
Kitupala <kimmo.tuupanen@gmail.com>
2023-07-25 12:26:31 +0300

    Initial commit

---
+++ src/features/cart/RemoveItem.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { removeItem } from './cartSlice';
+
+function RemoveItem({ pizzaId }) {
+  const dispatch = useDispatch();
+  return (
+    <Button type="small" onClick={() => dispatch(removeItem(pizzaId))}>
+      Remove
+    </Button>
+  );
+}
+
+export default RemoveItem;
